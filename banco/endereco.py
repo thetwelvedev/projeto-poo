ed06cebf247294d5c02c59a90cc88b4336cd397c
Leonardo Castro <twelve.programming@gmail.com>
2024-11-17 22:38:19 -0400

    Alterações no código e agora o método alterarEndereco usa as funções hasattr e setattr

--- banco/endereco.py
+++ banco/endereco.py
@@ -10,9 +10,6 @@ class Endereco:
         self.estado = estado
         self.cep = cep
 
-    def limparTerminal(self) -> None:
-        os.system('cls' if os.name == 'nt' else 'clear')
-
     def exibirEndereco(self) -> None:
         cepStr = str(self.cep) #Formatação do CEP
         print(f"""Endereço da Agência
@@ -25,65 +22,12 @@ Estado: {self.estado}
 Cep: {cepStr[:5]}-{cepStr[5:]}
 """)
 
-    def alterarEndereco(self) -> None:
-        while True:
-            self.limparTerminal()
-            op = int(input("""Qual dado deseja alterar:
-1 - Rua
-2 - Número da Casa
-3 - Bairro
-4 - Complemento
-5 - Cidade
-6 - Estado
-7 - CEP
-0 - Sair
-Digite sua escolha: """))
-            if op == 0:
-                    print("Edição encerrada.")
-                    break
-
-            match op:
-                case 1:
-                    self.rua = str(input("Digite o novo valor para Rua: "))
-                case 2:
-                    self.numeroDaCasa = int(input("Digite o novo valor para Número da Casa: "))
-                case 3:
-                    self.bairro = str(input("Digite o novo valor para Bairro: "))
-                case 4:
-                    self.complemento = str(input("Digite o novo valor para Complemento: "))
-                case 5:
-                    self.cidade = str(input("Digite o novo valor para Cidade: "))
-                case 6:
-                    self.estado = str(input("Digite o novo valor para Estado: "))
-                case 7:
-                    self.cep = int(input("Digite o novo valor para CEP: "))
-                case _:
-                    print("Opção inválida. Nenhuma alteração foi feita.")
-                    input("Pressione Enter para continuar...")
-
-    def menu(self, endereco) -> None:
-        while True:
-            endereco.limparTerminal()
-            op = int(input("""Escolha uma opção:
-1 - Exibir Endereço
-2 - Alterar Endereço
-0 - Sair
-Digite sua escolha: """))
-
-            match op:
-                case 1:
-                    endereco.exibirEndereco()
-                    input("Pressione Enter para voltar ao menu...")
-                case 2:
-                    endereco.alterarEndereco()
-                case 0:
-                    print("Saindo do sistema...")
-                    break
-                case _:
-                    print("Opção inválida. Nenhuma ação foi realizada.")
-                    input("Pressione Enter para continuar...")
-                
+    def alterarEndereco(self, atributo, valor):
+        if hasattr(self, atributo): #Verifica se tem o existe esse atributo no objeto
+            setattr(self, atributo, valor)#Faz o setter desse objeto seria equivalente a self.atributo = valor/Ele recebe o atributo como string
 
 if __name__ == '__main__':
     teste = Endereco('C', 361, 'Cidade Satélite', 'Casa Verde', 'Boa Vista', 'Roraima', 69317584)
-    teste.menu(teste)
+    teste.exibirEndereco()
+    teste.alterarEndereco('rua', 'F') #Vai ter que fazer lá no menu ele quando escolher por exemplo rua vim o parâmetro 'rua' para poder alterar
+    teste.exibirEndereco()
