019d8f804550cc99ac4dcb51895fb6d9cebb15ca
Alefe <alefealvesdacosta@gmail.com>
2025-02-24 18:34:51 -0400

    Implementando lógica base para os métodos de cadastro e login da classe Usuário

--- temporário/Desenvolvendo/classes_usuario.py
+++ temporário/Desenvolvendo/classes_usuario.py
@@ -4,7 +4,7 @@ class Usuario:
     """Classe usuário referente a pessoa com um cadstro básico no sistema.\n
     Atributos:\n Nome, cpf, email, telefone e senha todos do tipo string\n
     Métodos:\n Cadastrar, login e visualizar_historico"""
-    
+
     def __init__(self, nome: str, cpf: str, email: str, telefone: str, senha: str):
         """Construtor da classe usuários para criar um novo usuário"""
         self.nome = nome
@@ -19,14 +19,36 @@ class Usuario:
         hash_string = hash_string.encode("utf8")
         return hashlib.sha256(hash_string).hexdigest()
 
+    def _validar_senha(self, senha):
+        """Método para validar se a senha informada corresponde com a senha do usuário"""
+        criptografa_senha = self._cripitografa_senha(senha)
+        return criptografa_senha == self.senha
 
-    def cadastrar():
-        """Método cadastrar"""
-        pass
+    @classmethod
+    def cadastrar(cls, cpf, email, telefone, lista_usuarios):
+        """Método para verificar se é possivel realizar um cadastro com as credencias fornecidas.
+        Esse método futuramente será uma requisição ao banco de dados para verificar se já existe um usuário com as informações fornecidas."""
 
-    def login():
-        """Método login"""
-        pass
+        for usuario in lista_usuarios:
+            if(usuario.cpf == cpf or usuario.email == email or usuario.telefone == telefone):
+                print("Já existe um usuário com essas credenciais.")#Implementar uma execção de cadastro já existente
+            else:
+                print("Cadastro realizado com sucesso")
+
+
+
+    @classmethod
+    def login(cls, email, senha, list_usuarios):
+        """Método para validar a tentativa de login no sistema de um usuário.
+        Esse método futuramente será melhorado para fazer uma requisição ao banco de dados, 
+        afim de verificar se os dados corrrespondem a algum usuário."""
+        
+        
+        for usuario in list_usuarios:
+            if(usuario.email == email and usuario._validar_senha(senha)):
+                print("Acesso bem sucedido.")#Será uma validação enviada para o front-end validando o login
+            else:
+                print("Falha ao realizar login")#Implementar uma execção de erro de login aqui.
 
     def visualizar_historico_compras():
         pass
@@ -61,8 +83,8 @@ class Cliente(Usuario):
 
 class Administrador(Usuario):
     """Classe Administrador que herda de Usuario, referente a pessoa com cadastro de adm.\n
-      Atributos:\n Os herdados de Usuario e codigo_acesso.\n
-      Métodos:\n onde está pode realizar funções de manutenção do sistema."""
+      Atributos: Os herdados de Usuario e codigo_acesso.\n
+      Métodos: Onde está pode realizar funções de manutenção do sistema."""
     
     def __init__(self, nome, cpf, email, telefone, senha, codigo_acesso: str):
         super().__init__(nome, cpf, email, telefone, senha)
--- temporário/Desenvolvendo/sistema.py
+++ temporário/Desenvolvendo/sistema.py
@@ -1,16 +1,37 @@
 from Desenvolvendo import Usuario
 
+list_usuarios = []#
+
+
 def cadastrar():
-    nome = input("Informe seu nome")
-    cpf = input("Informe seu cpf")
-    email = input("Informe seu email")
-    tel = input("Informe seu telefone")
-    senha = input("Digite uma senha")
+    """Função auxiliar do sistema para criar um objeto usuário, com seus devidos atributos e salvar em um banco de dados.
+    Observe que está função será atualizada para receber uma requisição do front-end com os devidos atributos sendo enviados,
+    para a criação do objeto."""
+    
+    nome = input("Informe seu nome: ")
+    cpf = input("Informe seu cpf: ")
+    email = input("Informe seu email: ")
+    tel = input("Informe seu telefone: ")
+    senha = input("Digite uma senha: ")
 
     u = Usuario(nome, cpf, email, tel, senha)
+    list_usuarios.append(u)
 
+def login():
+    """Função auxiliar do sistema para verificar o acesso de um usuário já criado.
+    Essa função deve ser melhorada futuramente para ao invés de receber os dados do teclado,
+    receber uma requisição do front-end com os dados de email e senha a serem verificados."""
+    
+    #Esses dados futuramente serão pegos do front-end
+    email = input("Informe o email: ")
+    senha = input("Informe a senha: ")
+
+    Usuario.login(email, senha, list_usuarios)
+
+#teste das funções
 def sistema():
     cadastrar()
+    login()
 
 if __name__ == "__main__":
     sistema()
