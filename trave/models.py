80a841de5e706d636033e24f21f5c6bc8e078495
Leonardo Castro <twelve.programming@gmail.com>
2025-03-02 01:39:36 -0400

    Fazendo os dados do cadastro.html ir para o banco de dados

--- trave/models.py
+++ trave/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=50, unique=True)
+    senha = models.CharField(max_length=50)
+    primeiro_nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14, unique=True)  # CPF com 14 caracteres (incluindo pontos e traço)
+    telefone = models.CharField(max_length=15)  # Telefone com 15 caracteres (incluindo parênteses e traço)
+    numero_cartao = models.CharField(max_length=19)  # Número do cartão com 19 caracteres (incluindo espaços)
+    email = models.EmailField(unique=True)
+    endereco = models.CharField(max_length=255)
+    estado = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    cep = models.CharField(max_length=9)  # CEP com 9 caracteres (incluindo traço)
+
+    def __str__(self):
+        return self.usuario
--- trave/views.py
+++ trave/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UsuarioForm
 
 def cadastro_view(request):
-    return render(request, 'cadastro.html')
+    cadastro_sucesso = False  # Variável para indicar se o cadastro foi bem-sucedido
+
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()  # Salva os dados no banco de dados
+            cadastro_sucesso = True  # Define como True após o cadastro ser bem-sucedido
+            form = UsuarioForm()  # Limpa o formulário após o cadastro
+    else:
+        form = UsuarioForm()
+    
+    return render(request, 'cadastro.html', {'form': form, 'cadastro_sucesso': cadastro_sucesso})
 
 def login_view(request):
     return render(request, 'login.html')
