6bf4e8b7d387f33aa3cf54cccd957647910a9c05
Arthur Ramos <arthurcorreiaufrr@gmail.com>
2025-03-05 21:11:37 -0400

    implementação de metodos da classe compra

--- servicos/classe_compra.py
+++ servicos/classe_compra.py
@@ -12,11 +12,35 @@ class Compra:
         self.status = status
         self.metodo_pagamento = metodo_pagamento
 
-    def realizar_compra():
-        pass
+    def realizar_compra(self):
+        if self.status.lower() == 'pendente':
+            self.status = 'Confirmada'
+            print(f"Compra {self.codigo_compra} realizada com sucesso para o cliente {self.cliente.nome}.")
+        else:
+            print(f"Compra {self.codigo_compra} já está {self.status}.")
 
-    def cancelar_compra():
-        pass
 
-    def emitir_comprovante():
-        pass
+    def cancelar_compra(self):
+        if self.status.lower() == 'confirmada':
+            self.status = 'Cancelada'
+            print(f"Compra {self.codigo_compra} foi cancelada com sucesso.")
+        elif self.status.lower() == 'pendente':
+            self.status = 'Cancelada'
+            print(f"Compra {self.codigo_compra} foi cancelada antes da confirmação.")
+        else:
+            print(f"Compra {self.codigo_compra} não pode ser cancelada pois está {self.status}.")
+
+
+    def emitir_comprovante(self):
+        comprovante = (
+            f"===== Comprovante de Compra =====\n"
+            f"Codigo da Compra: {self.codigo_compra}\n"
+            f"Cliente: {self.cliente.nome} \n"
+            f"Voo: {self.voo.codigo_voo} Origem  {self.voo.origem} Destino  {self.voo.destino}\n"
+            f"Data da Compra: {self.data_compra}\n"
+            f"Valor Total: R$ {self.valor_total:.2f}\n"
+            f"Método de Pagamento: {self.metodo_pagamento}\n"
+            f"Status: {self.status}\n"
+            f"================================="
+        )
+        print(comprovante)
